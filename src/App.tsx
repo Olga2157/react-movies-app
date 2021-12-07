fd4e67bd85d1a858c834b01632808230b867dd16
Olga2157 <insight2057@mail.ru>
2021-12-07 19:15:25 +0300

    feat: add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import HomePage from "./components/HomePage/HomePage";
+import ErrorBoundary from './components/shared/ErrorBoundary';
 
 class App extends React.Component {
 
   render() {
       return (
-        <HomePage/>
+        <ErrorBoundary>
+          <HomePage/>
+        </ErrorBoundary>
       );
   }
 }
