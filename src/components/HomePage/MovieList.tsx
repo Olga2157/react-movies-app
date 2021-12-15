770fbd008eee1e323545e6bec3aaed53968e4c22
Olga Golyakova <insight2057@mail.ru>
2021-12-15 18:39:57 +0300

    fix key in rendering array with movies

--- src/components/HomePage/MovieList.tsx
+++ src/components/HomePage/MovieList.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {FC} from 'react';
 import { Container } from 'reactstrap';
 import { MovieInfo } from '../../types';
 import Movie from './Movie';
@@ -6,9 +6,8 @@ import Movie from './Movie';
 type MovieListProps = {
   movieId?: string;
 }
-type MovieListState = {}
-class MovieList extends React.Component<MovieListProps, MovieListState> {
-  private static movies: MovieInfo[] = [
+export const MovieList: FC<MovieListProps> = () => {
+  const movies: MovieInfo[] = [
     {
       id: 1,
       title: 'Star Wars: The Last Jedi',
@@ -64,17 +63,15 @@ class MovieList extends React.Component<MovieListProps, MovieListState> {
       posterPath: 'https://image.tmdb.org/t/p/w500/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg',
       year: 2017,
       genre: 'Fantasy',
-    },
+    }
   ];
 
-  render() {
     return (
       <Container>
-        {MovieList.movies.map((movie, id) => (
-          <div key={id} className="item d-flex flex-column movie-area"><Movie movieInfo={movie} /></div>
+        {movies.map((movie) => (
+          <div key={movie.id} className="item d-flex flex-column movie-area"><Movie movieInfo={movie} /></div>
         ))}
       </Container>
     );
   }
-}
 export default MovieList;
