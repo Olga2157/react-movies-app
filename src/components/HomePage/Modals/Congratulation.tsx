a7fa0ed68aff8a1367ab7e2117ad71e2696f76e0
Olga Goliakova <insight2057@mail.ru>
2022-02-02 12:52:12 +0300

    feat: add congratulation modal

---
+++ src/components/HomePage/Modals/Congratulation.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from 'react';
+import {
+    Modal, ModalBody, ModalHeader,
+} from 'reactstrap';
+import { useToggle } from 'react-use';
+import { AppText, Heading } from '../../shared';
+import './Modal.scss';
+
+export const Congratulation: FC = () => {
+    const [isOpen, toggleModal] = useToggle(false);
+
+    return (
+            <Modal
+                backdrop={false}
+                centered
+                fullscreen="md"
+                scrollable
+                size="md"
+                className="special-modal"
+                isOpen={isOpen}
+            >
+                <ModalHeader toggle={toggleModal}>
+                    <div className="d-flex flex-row align-items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" fill="#f65261" className="bi bi-check-circle-fill m-2" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                        </svg>
+                        <Heading headingText="Congratulations!" upperCase />
+                    </div>
+
+                </ModalHeader>
+                <ModalBody>
+                    <AppText text="The movie has been added to database successfully" />
+                </ModalBody>
+            </Modal>
+    );
+};
