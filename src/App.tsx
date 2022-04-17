536d7b962abbb22be64dd5e79b9fec3561d46497
Olga Golyakova <insight2057@mail.ru>
2022-04-17 17:22:54 +0300

    feat: add dinamic search query params to url

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import * as ReactDom from 'react-dom';
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ErrorBoundary from './components/shared/ErrorBoundary';
 import { HomePage } from './components/HomePage/HomePage';
@@ -11,15 +11,11 @@ export const App: FC = () => (
   <ErrorBoundary>
     <Router>
       <Switch>
-        
         <Route path="/film/:id" component={ MovieDetailsPage } />
+        <Route path="/search" component={ HomePage } />
         <Route exact path="/" component={ HomePage } />
         <Route path="*" component={ NotFoundPage } />
       </Switch>
-      {/* <Redirect to="/" /> */}
-      {/* <Redirect exact from="/" to="/film/:id" /> */}
-      {/* <Redirect exact from="*" to="/" /> */}
     </Router>
-
   </ErrorBoundary>
 );
